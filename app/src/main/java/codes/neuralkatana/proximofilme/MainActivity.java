6eeeb721b4347e3d9e0a4e768abe0975c4b0b383
neuralrunner <brunohcagnin@gmail.com>
2020-05-18 16:30:36 -0300

    Criação do projeto: ProximoFilme.

---
+++ app/src/main/java/codes/neuralkatana/proximofilme/MainActivity.java
@@ -0,0 +1,14 @@
+package codes.neuralkatana.proximofilme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
