4f063143c52478e6fdc89d5c88984fc149d3db5e
neuralrunner <brunohcagnin@gmail.com>
2020-06-16 16:36:32 -0300

    Adição do FilmeDetalheFragment realizando uma migração da FilmeDetalheActivity.

--- app/src/main/java/codes/neuralkatana/proximofilme/activities/FilmeDetalheActivity.java
+++ app/src/main/java/codes/neuralkatana/proximofilme/activities/FilmeDetalheActivity.java
@@ -1,6 +1,8 @@
 package codes.neuralkatana.proximofilme.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,6 +11,7 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 
 import codes.neuralkatana.proximofilme.R;
+import codes.neuralkatana.proximofilme.fragments.FilmeDetalheFragment;
 import codes.neuralkatana.proximofilme.pojos.ItemFilme;
 
 public class FilmeDetalheActivity extends AppCompatActivity {
@@ -17,20 +20,15 @@ public class FilmeDetalheActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filme_detalhe);
-
         Intent intent = getIntent();
         ItemFilme itemFilme = (ItemFilme) intent.getSerializableExtra(MainActivity.KEY_FILME);
 
-        TextView titulo = findViewById(R.id.item_titulo);
-        TextView desc = findViewById(R.id.item_desc);
-        TextView data = findViewById(R.id.item_data);
-        RatingBar aval = findViewById(R.id.item_avaliacao);
-        Button btnTrailer = findViewById(R.id.item_btn_trailer);
-
-        titulo.setText(itemFilme.getTitulo());
-        desc.setText(itemFilme.getDescricao());
-        data.setText(itemFilme.getDataLancamento());
-        aval.setRating(itemFilme.getAvaliacao());
-
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        FilmeDetalheFragment fragment = new FilmeDetalheFragment();
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(MainActivity.KEY_FILME, itemFilme);
+        fragment.setArguments(bundle);
+        fragmentTransaction.commit();
     }
 }
---
+++ app/src/main/java/codes/neuralkatana/proximofilme/fragments/FilmeDetalheFragment.java
@@ -0,0 +1,53 @@
+package codes.neuralkatana.proximofilme.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import codes.neuralkatana.proximofilme.R;
+import codes.neuralkatana.proximofilme.activities.MainActivity;
+import codes.neuralkatana.proximofilme.pojos.ItemFilme;
+
+public class FilmeDetalheFragment extends Fragment {
+
+    private ItemFilme itemFilme;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if(getArguments() != null){
+            itemFilme = (ItemFilme) getArguments().getSerializable(MainActivity.KEY_FILME);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_filme_detalhe, container, false);
+
+        if(itemFilme == null){
+            return view;
+        }
+
+        TextView titulo = view.findViewById(R.id.item_titulo);
+        TextView desc = view.findViewById(R.id.item_desc);
+        TextView data = view.findViewById(R.id.item_data);
+        RatingBar aval = view.findViewById(R.id.item_avaliacao);
+        Button btnTrailer = view.findViewById(R.id.item_btn_trailer);
+
+        titulo.setText(itemFilme.getTitulo());
+        desc.setText(itemFilme.getDescricao());
+        data.setText(itemFilme.getDataLancamento());
+        aval.setRating(itemFilme.getAvaliacao());
+
+        return view;
+    }
+}
